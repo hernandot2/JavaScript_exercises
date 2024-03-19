eca456e3335a4290be2ebc7d15e6d59d2821e98a
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2024-03-19 10:13:10 +0900

    first codes

---
+++ script.js
@@ -0,0 +1,10 @@
+var nome = 'tamon';
+var idade = 46;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco; 
+
+console.log(totalPreco);
